82624b73edfdbfee2a38010c2e4eb73eb763d866
blakecrowther <blakecrowther7@gmail.com>
2020-06-11 19:42:51 -0700

    Finished logic for project requirements with random numbers as placeholders

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StatusBar, StyleSheet} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
 import EthereumWallet from './components/EthereumWallet/EthereumWallet';
 
--- components/EthereumWallet/EthereumWallet.js
+++ components/EthereumWallet/EthereumWallet.js
@@ -1,11 +1,66 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import DeviceInfo from 'react-native-device-info';
 
 import styles from './EthereumWallet.styles';
 
+// const wallet = require('eth-wallet-light');
+
 export default function EthereumWallet() {
-  const [publicKey, setPublicKey] = useState('public');
-  const [privateKey, setPrivateKey] = useState('private');
+  const [publicKey, setPublicKey] = useState('');
+  const [privateKey, setPrivateKey] = useState('');
+
+  const password = 'password';
+
+  // async () => {
+
+  // };
+
+  useEffect(() => {
+    const checkStorage = async () => {
+      try {
+        const storedPublicKey = await AsyncStorage.getItem('asyncPublicKey');
+        const storedPrivateKey = await AsyncStorage.getItem('asyncPrivateKey');
+        if (storedPublicKey && storedPrivateKey) {
+          setPublicKey(storedPublicKey);
+          setPrivateKey(storedPrivateKey);
+        } else {
+          getNewAddress();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    checkStorage();
+  }, []);
+
+  getNewAddress = async () => {
+    try {
+      // var keystore = await new wallet.Keystore()
+      //   .initializFromEntropy('entropy', password)
+      //   .then(console.log('Address: ', keystore.getAddress()));
+      //   // generateSecureRandom(32
+      //   // ).then((randomBytes) =>
+      //   //   randomBytes.join('').toString(),
+      //   // ),
+      //   // console.log('privateKey: ', keystore.getPrivateKey(password));
+      const newPublicKey = `${Math.random() * 100000000}`;
+      const newPrivateKey = `${Math.random() * 100000000}`;
+      setPublicKey(newPublicKey);
+      setPrivateKey(newPrivateKey);
+      await AsyncStorage.setItem('asyncPublicKey', newPublicKey);
+      await AsyncStorage.setItem('asyncPrivateKey', newPrivateKey);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  clearAsyncStorage = async () => {
+    console.log(await AsyncStorage.getItem('asyncPublicKey'));
+    await AsyncStorage.clear();
+  }
+
 
   return (
     <View style={styles.Container}>
@@ -28,7 +83,7 @@ export default function EthereumWallet() {
           />
         </View>
       </View>
-      <TouchableOpacity style={styles.Button}>
+      <TouchableOpacity style={styles.Button} onPress={getNewAddress}>
         <Text style={styles.ButtonText}>Generate a new one</Text>
       </TouchableOpacity>
     </View>
